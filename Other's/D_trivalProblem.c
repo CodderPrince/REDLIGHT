a2fe9dccb62b26da8691b342034d74e55e2af9d4
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:18:54 +0600

    Create D_trivalProblem.c

---
+++ Other's/D_trivalProblem.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    scanf("%d",&m);
+    int num,x,sum=0;
+    for(int i=1; i<=100000; i++)
+    {
+        num=1;
+        x=i;
+        while(1)
+        {
+            if(x%5==0)
+            {
+                num++;
+                x= x/5;
+            }
+            else
+            {
+                break;
+            }
+        }
+        sum= sum+num;
+        if(sum==m)
+        {
+            printf("5\n");
+            for(int j=5*i; j<5*i+5; j++)
+            {
+                printf("%d ",j);
+            }
+            return 0;
+        }
+        else if(sum>m)
+        {
+            break;
+        }
+    }
+    printf("0");
+    return 0;
+}

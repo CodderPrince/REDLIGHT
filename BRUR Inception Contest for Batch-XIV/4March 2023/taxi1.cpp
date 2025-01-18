0bdef16def3b01f32c933d3c3d89040ffbdf9cf1
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:18:51 +0600

    Create taxi1.cpp

---
+++ BRUR Inception Contest for Batch-XIV/4March 2023/taxi1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, c, count[5]= {0};
+    scanf("%d", &n);
+    while (n--)
+    {
+        scanf("%d", &c);
+        count[c] ++;
+    }
+    int total = count[4] + count[3] + count[2] / 2;
+    count[1] = count[1] - count[3];
+    if (count[2] % 2 == 1)
+    {
+        total = total + 1;
+        count[1] = count[1] - 2;
+    }
+    if (count[1] > 0)
+    {
+        total = total + (count[1] + 3) / 4;
+    }
+    printf("%d\n", total);
+    return 0;
+}
